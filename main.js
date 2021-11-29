afd6eb172ac40743a3249a3ae7193bafeccff76a
Swarali <83956995+Swarali735-ai@users.noreply.github.com>
2021-11-29 11:42:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+nose_X = 0;
+nose_Y = 0;
+difference = 0;
+
+leftWristX = 0;
+rightWristY = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,600);
+    video.position(10,60);
+
+
+    canvas =  createCanvas(600,440);
+    canvas.position(600,130);
+
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+
+
+
+
+    poseNet.on('pose', gotPoses);
+}
+function gotPoses(results) {
+    if (results.length > 0) 
+    {
+         console.log(results);
+    } 
+
+
+     leftWristX = results[0].pose.leftWrist.x;
+     rightWristY = results[0].pose.rightWrist.x;
+
+     difference = floor(leftWristX - rightWristY);
+     console.log(" left wrist = " + leftWristX + " right wrist = " + rightWristY + " difference =  " +  difference);
+    
+}
+
+function modelLoaded() {
+    console.log('poseNet is intialized');
+}
+
+
+function draw(){
+  background('#800080');
+  textSize(difference);
+  fill('#673147');
+  text('Swarali',50,400);
+
+}
